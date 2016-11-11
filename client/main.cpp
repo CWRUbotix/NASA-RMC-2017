cede0fe760362ac47cf36761022eea9c6c3d40d8
xiangxue <xxw345@case.edu>
2016-11-11 17:50:59 -0500

    initGUI

--- client/main.cpp
+++ client/main.cpp
@@ -1,73 +1,75 @@
 #include "mainwindow.h"
-#include "connectiondialog.h"
+//#include "connectiondialog.h"
 #include <QApplication>
 #include <QDebug>
-#include <amqp_tcp_socket.h>
-#include <amqp.h>
-#include <amqp_framing.h>
-#include "amqp_utils.h"
-#include <QMessageBox>
+//#include <amqp_tcp_socket.h>
+//#include <amqp.h>
+//#include <amqp_framing.h>
+//#include "amqp_utils.h"
+//#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    amqp_connection_state_t conn = amqp_new_connection();
-    amqp_socket_t *socket = amqp_tcp_socket_new(conn);
+//    amqp_connection_state_t conn = amqp_new_connection();
+//    amqp_socket_t *socket = amqp_tcp_socket_new(conn);
 
-    if (!socket) {
-      QMessageBox::critical(0,"Error","Failed to allocate socket.\nDetails: amqp_tcp_socket_new() returned NULL");
-      return 1;
-    }
+//    if (!socket) {
+//      QMessageBox::critical(0,"Error","Failed to allocate socket.\nDetails: amqp_tcp_socket_new() returned NULL");
+//      return 1;
+//    }
 
-    ConnectionDialog d;
-    d.exec();
-    if (d.result() != QDialog::Accepted) {
-        return 0;
-    }
+//    ConnectionDialog d;
+//    d.exec();
+//    if (d.result() != QDialog::Accepted) {
+//        return 0;
+//    }
 
-    QString address = d.URL();
+//    QString address = d.URL();
 
-    int status = amqp_socket_open(socket, address.toStdString().c_str(), 5672);
-    if (AMQP_STATUS_OK != status) {
-        QMessageBox::critical(0,"Error","Failed to open socket.\nDetails: amqp_socket_open() returned: " + QString::number(status));
-        return 1;
-    }
+//    int status = amqp_socket_open(socket, address.toStdString().c_str(), 5672);
+//    if (AMQP_STATUS_OK != status) {
+//        QMessageBox::critical(0,"Error","Failed to open socket.\nDetails: amqp_socket_open() returned: " + QString::number(status));
+//        return 1;
+//    }
 
-    amqp_rpc_reply_t reply = amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
-    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
-        QMessageBox::critical(0,"Error","Failed to log in to server.\nDetails: amqp_login() returned: " + amqp_error_message(reply));
-        return 1;
-    }
-    amqp_channel_open(conn, 1);
-    reply = amqp_get_rpc_reply(conn);
-    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
-        QMessageBox::critical(0,"Error","Failed to open channel.\nDetails: amqp_get_rpc_reply() returned: " + amqp_error_message(reply));
-        return 1;
-    }
+//    amqp_rpc_reply_t reply = amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
+//    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
+//        QMessageBox::critical(0,"Error","Failed to log in to server.\nDetails: amqp_login() returned: " + amqp_error_message(reply));
+//        return 1;
+//    }
+//    amqp_channel_open(conn, 1);
+//    reply = amqp_get_rpc_reply(conn);
+//    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
+//        QMessageBox::critical(0,"Error","Failed to open channel.\nDetails: amqp_get_rpc_reply() returned: " + amqp_error_message(reply));
+//        return 1;
+//    }
 
-    MainWindow w(conn);
+    //MainWindow w(conn);
+    MainWindow w;
     w.show();
 
     int output = a.exec();
 
-    reply = amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
-    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
-        QMessageBox::critical(0,"Error","Failed to close channel.\nDetails: amqp_channel_close() returned: " + amqp_error_message(reply));
-        return 1;
-    }
+//    reply = amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
+//    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
+//        QMessageBox::critical(0,"Error","Failed to close channel.\nDetails: amqp_channel_close() returned: " + amqp_error_message(reply));
+//        return 1;
+//    }
 
-    reply = amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
-    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
-        QMessageBox::critical(0,"Error","Failed to close connection.\nDetails: amqp_connection_close() returned: " + amqp_error_message(reply));
-        return 1;
-    }
+//    reply = amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
+//    if (AMQP_RESPONSE_NORMAL != reply.reply_type) {
+//        QMessageBox::critical(0,"Error","Failed to close connection.\nDetails: amqp_connection_close() returned: " + amqp_error_message(reply));
+//        return 1;
+//    }
 
-    status = amqp_destroy_connection(conn);
-    if (AMQP_STATUS_OK != status) {
-        QMessageBox::critical(0,"Error","Failed to destruct connection.\nDetails: amqp_destroy_connection() returned: " + QString::number(status));
-        return 1;
-    }
+//    status = amqp_destroy_connection(conn);
+//    if (AMQP_STATUS_OK != status) {
+//        QMessageBox::critical(0,"Error","Failed to destruct connection.\nDetails: amqp_destroy_connection() returned: " + QString::number(status));
+//        return 1;
+//    }
 
     return output;
+//    return  1;
 }
--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -12,69 +12,69 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QObject::connect(ui->locomotion_UpButton, &QPushButton::clicked,
-                     this, &MainWindow::handleLocomotionUp);
-    QObject::connect(ui->locomotion_DownButton, &QPushButton::clicked,
-                     this, &MainWindow::handleLocomotionDown);
-    QObject::connect(ui->locomotion_LeftButton, &QPushButton::clicked,
-                     this, &MainWindow::handleLocomotionLeft);
-    QObject::connect(ui->locomotion_RightButton, &QPushButton::clicked,
-                     this, &MainWindow::handleLocomotionRight);
+//    QObject::connect(ui->locomotion_UpButton, &QPushButton::clicked,
+//                     this, &MainWindow::handleLocomotionUp);
+//    QObject::connect(ui->locomotion_DownButton, &QPushButton::clicked,
+//                     this, &MainWindow::handleLocomotionDown);
+//    QObject::connect(ui->locomotion_LeftButton, &QPushButton::clicked,
+//                     this, &MainWindow::handleLocomotionLeft);
+//    QObject::connect(ui->locomotion_RightButton, &QPushButton::clicked,
+//                     this, &MainWindow::handleLocomotionRight);
 }
 
-MainWindow::MainWindow(amqp_connection_state_t conn, QWidget *parent) :
-    MainWindow::MainWindow(parent)
-{
-    m_conn = conn;
-}
+//MainWindow::MainWindow(amqp_connection_state_t conn, QWidget *parent) :
+//    MainWindow::MainWindow(parent)
+//{
+//    m_conn = conn;
+//}
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
-void MainWindow::handleLocomotion(LocomotionControl_LocomotionType direction) {
-    LocomotionControl msg;
-    msg.set_locomotiontype(direction);
-    msg.set_speed_percent(ui->locomotion_SpeedSlider->value());
-    msg.set_timeout_ms(ui->locomotion_DurationSlider->value());
-    int msg_size = msg.ByteSize();
-    void *msg_buff = malloc(msg_size);
-    if (!msg_buff) {
-        ui->consoleOutputTextBrowser->append("Failed to allocate message buffer.\nDetails: malloc(msg_size) returned: NULL\n");
-        return;
-    }
-    msg.SerializeToArray(msg_buff, msg_size);
-    amqp_bytes_t message_bytes;
-    message_bytes.len = msg_size;
-    message_bytes.bytes = msg_buff;
-    const char* queue_name = "locomotion";
-    int status = amqp_basic_publish(m_conn,
-                                    1,
-                                    amqp_cstring_bytes(""),
-                                    amqp_cstring_bytes(queue_name),
-                                    0,
-                                    0,
-                                    NULL,
-                                    message_bytes);
-    if (AMQP_STATUS_OK != status) {
-        ui->consoleOutputTextBrowser->append("Failed to publish message.\nDetails: amqp_basic_publish() returned: " + QString::number(status) + "\n");
-    }
-    free(msg_buff);
-}
+//void MainWindow::handleLocomotion(LocomotionControl_LocomotionType direction) {
+//    LocomotionControl msg;
+//    msg.set_locomotiontype(direction);
+// //   msg.set_speed_percent(ui->locomotion_SpeedSlider->value());
+// //   msg.set_timeout_ms(ui->locomotion_DurationSlider->value());
+//    int msg_size = msg.ByteSize();
+//    void *msg_buff = malloc(msg_size);
+//    if (!msg_buff) {
+//        ui->consoleOutputTextBrowser->append("Failed to allocate message buffer.\nDetails: malloc(msg_size) returned: NULL\n");
+//        return;
+//    }
+//    msg.SerializeToArray(msg_buff, msg_size);
+//    amqp_bytes_t message_bytes;
+//    message_bytes.len = msg_size;
+//    message_bytes.bytes = msg_buff;
+//    const char* queue_name = "locomotion";
+//    int status = amqp_basic_publish(m_conn,
+//                                    1,
+//                                    amqp_cstring_bytes(""),
+//                                    amqp_cstring_bytes(queue_name),
+//                                    0,
+//                                    0,
+//                                    NULL,
+//                                    message_bytes);
+//    if (AMQP_STATUS_OK != status) {
+//        ui->consoleOutputTextBrowser->append("Failed to publish message.\nDetails: amqp_basic_publish() returned: " + QString::number(status) + "\n");
+//    }
+//    free(msg_buff);
+//}
 
-void MainWindow::handleLocomotionUp() {
-    handleLocomotion(LocomotionControl_LocomotionType_FORWARD);
-}
+//void MainWindow::handleLocomotionUp() {
+//    handleLocomotion(LocomotionControl_LocomotionType_FORWARD);
+//}
 
-void MainWindow::handleLocomotionDown() {
-    handleLocomotion(LocomotionControl_LocomotionType_BACKWARD);
-}
+//void MainWindow::handleLocomotionDown() {
+//    handleLocomotion(LocomotionControl_LocomotionType_BACKWARD);
+//}
 
-void MainWindow::handleLocomotionLeft() {
-    handleLocomotion(LocomotionControl_LocomotionType_LEFT);
-}
+//void MainWindow::handleLocomotionLeft() {
+//    handleLocomotion(LocomotionControl_LocomotionType_LEFT);
+//}
 
-void MainWindow::handleLocomotionRight() {
-    handleLocomotion(LocomotionControl_LocomotionType_RIGHT);
-}
+//void MainWindow::handleLocomotionRight() {
+//    handleLocomotion(LocomotionControl_LocomotionType_RIGHT);
+//}
