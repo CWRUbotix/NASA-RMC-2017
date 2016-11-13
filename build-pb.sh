d8818ab88335f13bae46080096a9c85cbfb3417a
Michael Schaffer <mls278@case.edu>
2016-11-13 01:17:47 -0500

    Put generated files in directories

--- build-pb.sh
+++ build-pb.sh
@@ -2,4 +2,5 @@
 
 # This script builds all the protobuf classes for java, cpp, and python
 
-protoc --cpp_out=. --java_out=. --python_out=. messages.proto
+mkdir -p pb
+protoc --cpp_out=pb/. --java_out=pb/. --python_out=pb/. --python_out=glennifer/. messages.proto
