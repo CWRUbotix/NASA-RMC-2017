6ec18264ada07779a38782ebf1c2931bee90e892
Anno van den Akker <annovda@gmail.com>
2016-11-18 01:27:40 -0500

    Coordinated motion

--- glennifer/HCI/cpp/include/HardwareControlInterface.hpp
+++ glennifer/HCI/cpp/include/HardwareControlInterface.hpp
@@ -10,6 +10,11 @@
 
 namespace hci {
 
+// Function that takes in data from coordinated actuator and
+// returns a target data point.  Used in coordinated actuations
+typedef double(*coord_map) (double);
+
+// Types of actuation
 enum ActuationType {
 	AngVel, AngPos,
 	LinVel, LinPos,
@@ -20,30 +25,81 @@ enum ActuationType {
 }
 
 struct ActuationConstraint {
+	// Type of actuation
 	ActuationType type;
+	// True for maximum limit
+	// False for minimum limit
 	bool max;
+	// Value of constraint
 	double value;
 }
 
 struct Actuation {
-	bool ovr, hold;
+	// Override previous actuations for this actuator
+	bool ovr;
+	// Hold target value
+	// If false, actuation is removed when completed
+	bool hold;
+	// Actuator of this actuation
 	uint8_t actuator;
+	// Type of actuation
 	ActuationType type;
-	double value;
+	// Target actuation
+	double targetValue;
 }
+
+struct CoordinatedActuation {
+	// Override previous actuations for this actuator
+	bool ovr;
+	// Hold target value
+	// If false, actuation is removed when completed
+	bool hold;
+	// Actuator of this actuation
+	uint8_t actuator;
+	// Coordinated actuator (actuator move relative to this)
+	uint8_t coordActuator;
+	// Type of coordinated actuation
+	ActuationType type;
+	// Defines relationship between target output and output of coordActuator
+	coord_map target;
+}
+
+// Generates a linear map between actuators
+coord_map genLinearMap(double offset, double scalar);
 
 class HardwareControlInterface {
 	private:
-		volatile bool running, stop, queue_lock;
+		// States whether hardware control interface thread is running
+		// Setting false will stop the hardware control interface
+		volatile bool running;
+		// Lock on actuationQueue to prevent multiple threads from
+		// accessing it at the same time
+		volatile bool queue_lock;
+		// Queue of actuations to be processed
 		std::queue<Actuation> actuationQueue;
+		// Queue of coordinated actuations to be processed
+		std::queue<CoordinatedActuation> coordActuationQueue;
+		// Vector of constraints
 		std::vector<ActuationConstraint> constraints;
-
+		// Map from actuator ID to actuator
 		std::unordered_map<uint8_t, Actuator> actuators;
+		// Map from sensor ID to sensor
 		std::unordered_map<uint8_t, Sensor> sensors;
-
+		// Vector of all active actuations
+		std::vector<Actuation> activeActuations;
+		// Vector of all coordinated actuations
+		std::vector<CoordinatedActuation> activeCoordActuations;
+		
 	public:
+		// Add an actuation to the queue
+		// Will reject actuations if they conflict and are not overriding
 		int queueActuation(Actuation actuation);
+		// Add a coordinated actuation to the queue
+		// Will reject actuations if they conflict and are not overriding
+		int queueCoordinatedActuation(CoordinatedActuation coordActuation);
+		// Halts the hardware control interface
 		void halt();
+		// Function for thread of hardware control interface
 		void hardwareInterface();
 }
 
