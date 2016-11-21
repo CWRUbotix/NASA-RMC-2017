28a27ea0cfbbb80a37ec706f04ba10e3720ee37a
Michael Schaffer <mls278@case.edu>
2016-11-21 17:06:19 -0500

    Import amqpcpp correctly

--- client/conanfile.py
+++ client/conanfile.py
@@ -2,6 +2,6 @@ from conans import ConanFile, CMake
 
 class GlenniferClientConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "librabbitmq/0.8.1@filonovpv/stable", "amqpcpp/2.6.2@theirix/stable"#, "Protobuf/2.6.1@memsharded/testing"
+   requires = "librabbitmq/0.8.1@filonovpv/stable", "amqp/1.0-snapshot@ichaelm/testing"#, "Protobuf/2.6.1@memsharded/testing"
    generators = "qmake"
    build_policy = "missing"
