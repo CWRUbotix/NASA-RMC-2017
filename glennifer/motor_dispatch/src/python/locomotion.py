309f6acb2ec42206d5846fecaa2031fed5f9561e
Michael Schaffer <mls278@case.edu>
2017-02-04 20:12:55 -0500

    Make locomotion dispatch actually work

--- glennifer/motor_dispatch/src/python/locomotion.py
+++ glennifer/motor_dispatch/src/python/locomotion.py
@@ -1,177 +1,165 @@
 import pika
 import messages_pb2
+from enum import Enum
 
-# Create global wheel speed and pod
-front_left_speed = message_pb2.SpeedControlCommand()
-front_right_speed = message_pb2.SpeedControlCommand()
-back_left_speed = message_pb2.SpeedControlCommand()
-back_right_speed = message_pb2.SpeedControlCommand()
-
-front_left_pod = message_pb2.PositionControlCommand()
-front_right_pod = message_pb2.PositionControlCommand()
-back_left_pod = message_pb2.PositionControlCommand()
-back_right_pod = message_pb2.PositionControlCommand()
-
-configure = message_pb2.LocomotionControlCommandConfigure()
+class Wheel(Enum):
+    front_left = 1
+    front_right = 2
+    back_left = 3
+    back_right = 4
 
 # Declare 4 queue names and corresponding 4 binding keys
-straight_queue = 'straight'
-turn_queue = 'turn'
-strafe_queue = 'strafe'
-configure_queue = 'configure'
-
-straight_binding_key = 'subsystem.locomotion.straight'
-straight_binding_key = 'subsystem.locomotion.turn'
-straight_binding_key = 'subsystem.locomotion.strafe'
-straight_binding_key = 'subsystem.locomotion.configure'
+straight_queue = 'subsyscommand.locomotion.straight'
+turn_queue = 'subsyscommand.locomotion.turn'
+strafe_queue = 'subsyscommand.locomotion.strafe'
+configure_queue = 'subsyscommand.locomotion.configure'
 
 # Declare exchange from subsys
-exchange_name = ''
-
-# Declare exchange for emitting message
-exchange_emit = 'locomotion_hardware'
-
-# Declare configuration 
-configuration = message_pb2.LocomotionControlCommandConfigure()
+exchange_name = 'amq.topic'
 
 channel = None
 
 def on_connected(connection):
-	# Called when we establish a connection to RabbitMQ server
-	connection.channel(on_channel_open)
+    # Called when we establish a connection to RabbitMQ server
+    connection.channel(on_channel_open)
 
 def on_channel_open(new_channel):
-	# Called when establish a channel
-	global channel
-	channel = new_channel
-
-	# the channel receive one message at a time
-	channel.basic_qos(prefetch_count=1)
-
+    # Called when establish a channel
+    global channel
+    channel = new_channel
+    
   # declare and bind straight queue
-	channel.queue_declare(queue=straight_queue, durable=True,
-			exclusive=False, auto_delete=False)
-	channel.queue_bind(exchange=exchange_name, queue=straight_queue, 
-			routing_key=straight_binding_key)
-	# declare and bind turn queue
-	channel.queue_declare(queue=turn_queue, durable=True,
-			exclusive=False, auto_delete=False)
-	channel.queue_bind(exchange=exchange_name, queue=turn_queue,
-			routing_key=turn_binding_key)
+    channel.queue_declare(on_straight_queue_declare, straight_queue, durable=True,
+            exclusive=False, auto_delete=False)
+    # declare and bind turn queue
+    channel.queue_declare(on_turn_queue_declare, turn_queue, durable=True,
+            exclusive=False, auto_delete=False)
   # declare and bind strafe queue
-	channel.queue_declare(queue=strafe_queue, durable=True,
-			exclusive=False, auto_delete=False)
-	channel.queue_bind(exchange=exchange_name, queue=strafe_queue,
-			routing_key=strafe_binding_key)
-	# declare and bind strafe queue
-	channel.queue_declare(queue=configure_queue, durable=True,
-			exclusive=False, auto_delete=False)
-	channel.queue_bind(exchange=exchange_name, queue=configure_queue,
-			routing_key=configure_binding_key)
+    channel.queue_declare(on_strafe_queue_declare, strafe_queue, durable=True,
+            exclusive=False, auto_delete=False)
+    # declare and bind strafe queue
+    channel.queue_declare(on_configure_queue_declare, configure_queue, durable=True,
+            exclusive=False, auto_delete=False)
 
-	# create exchange emitting messages to hardware
-	channel.exchange_declare(exchange=exchange_emit,
-			type='topic')
-	
-	on_queue_bind()
+def on_straight_queue_declare(frame):
+    # Called when our queue is declared
+    channel.queue_bind(callback=on_straight_queue_bind, queue=straight_queue, exchange=exchange_name, routing_key=straight_queue)
 
-def on_queue_bind(frame):
-	# Called when our queue is declared and binded
-	channel.basic_consume(handle_straight, queue=straight_queue, no_ack=True)
-	channel.basic_consume(handle_turn, queue=turn_queue, no_ack=True)
-	channel.basic_consume(handle_strafe, queue=strafe_queue, no_ack=True)
-	channel.basic_consume(handle_configure, queue=configure_queue, no_ack=True)
+def on_turn_queue_declare(frame):
+    # Called when our queue is declared
+    channel.queue_bind(callback=on_turn_queue_bind, queue=turn_queue, exchange=exchange_name, routing_key=turn_queue)
+
+def on_strafe_queue_declare(frame):
+    # Called when our queue is declared
+    channel.queue_bind(callback=on_strafe_queue_bind, queue=strafe_queue, exchange=exchange_name, routing_key=strafe_queue)
+
+def on_configure_queue_declare(frame):
+    # Called when our queue is declared
+    channel.queue_bind(callback=on_configure_queue_bind, queue=configure_queue, exchange=exchange_name, routing_key=configure_queue)
+
+def on_straight_queue_bind(frame):
+    # Called when our queue is binded
+    channel.basic_consume(handle_straight, queue=straight_queue, no_ack=True)
+
+def on_turn_queue_bind(frame):
+    # Called when our queue is binded
+    channel.basic_consume(handle_turn, queue=turn_queue, no_ack=True)
+
+def on_strafe_queue_bind(frame):
+    # Called when our queue is binded
+    channel.basic_consume(handle_strafe, queue=strafe_queue, no_ack=True)
+
+def on_configure_queue_bind(frame):
+    # Called when our queue is binded
+    channel.basic_consume(handle_configure, queue=configure_queue, no_ack=True)
 
 def handle_straight(channel, method, header, body):
-	# Called when we receive messages from straight queue
-	msg_in = message_pb2.LocomotionControlCommandStraight()
-	msg_in.ParseFromString(body)
+    # Called when we receive messages from straight queue
+    msg_in = messages_pb2.LocomotionControlCommandStraight()
+    msg_in.ParseFromString(body)
 
-	# call out error if the configuration is not STRAIGHT yet
-	if (configure.target is not 
-			message_pb2.LocomotionCommandConfigure.Configuration.STRAIGHT_CONFIG)
-		# call out error
-
-	# else if the configuration is correct 
-	# set the wheels rpm and timeout
-	setup_rpm_timeout(front_left_speed, msg_in.rpm, msg_in.timeout)
-	setup_rpm_timeout(front_right_speed, msg_in.rpm, msg_in.timeout)
-	setup_rpm_timeout(back_left_speed, msg_in.rpm, msg_in.timeout)
-	setup_rpm_timeout(back_right_speed, msg_in.rpm, msg_in.timeout)
-	
+    # else if the configuration is correct 
+    # set the wheels rpm and timeout
+    publish_wheel_rpm(Wheel.front_left, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(Wheel.front_right, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(Wheel.back_left, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(Wheel.back_right, msg_in.speed, msg_in.timeout)
+    
 def handle_turn(channel, method, header, body):
-	# Called when we receive messages from turn queue
-	msg_in = message_pb2.LocomotionControlCommandTurn()
-	msg_in.ParseFromString(body)
+    # Called when we receive messages from turn queue
+    msg_in = messages_pb2.LocomotionControlCommandTurn()
+    msg_in.ParseFromString(body)
 
-	# call out error if the configuration is not TURN yet
-	if (configuration.target is not 
-			message_pb2.LocomotionCommandConfigure.Configuration.TURN_CONFIG)
-		# call out error
-
-	# else if the configuration is correct
-	setup_rpm_timeout(front_left_speed, msg_in.rpm, msg_in.timeout, 'motorcontrol.locomotion.front_left_wheel_rpm')
-	setup_rpm_timeout(front_right_speed, msg_in.rpm, msg_in.timeout, 'motorcontrol.locomotion.front_right_wheel_rpm')
-	setup_rpm_timeout(back_left_speed, msg_in.rpm, msg_in.timeout, 'motorcontrol.locomotion.back_left_wheel_rpm')
-	setup_rpm_timeout(back_right_speed, msg_in.rpm, msg_in.timeout, 'motorcontrol.locomotion.back_right_wheel_rpm')
-	
+    # else if the configuration is correct
+    publish_wheel_rpm(Wheel.front_left, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(Wheel.front_right, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(Wheel.back_left, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(Wheel.back_right, msg_in.speed, msg_in.timeout)
+    
 def handle_strafe(channel, method, header, body):
   # Called when we receive message from strafe queue
-	msg_in = message_pb2.LocomotionControlCommandStrafe()
-	msg_in.ParseFromString(body)
+    msg_in = messages_pb2.LocomotionControlCommandStrafe()
+    msg_in.ParseFromString(body)
 
-	# call out error if the configuration is not STRAFE yet
-	if (configuration.target is not 
-			message_pb2.LocomotionCommandConfigure.Configuration.TURN_CONFIG)
-		# call out error
-
-	# else if the configuration is correct
-	setup_rpm_timeout(front_left_speed, msg_in.rpm, msg_in.timeout)
-	setup_rpm_timeout(front_right_speed, msg_in.rpm, msg_in.timeout)
-	setup_rpm_timeout(back_left_speed, msg_in.rpm, msg_in.timeout)
-	setup_rpm_timeout(back_right_speed, msg_in.rpm, msg_in.timeout)
+    # else if the configuration is correct
+    publish_wheel_rpm(front_left_speed, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(front_right_speed, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(back_left_speed, msg_in.speed, msg_in.timeout)
+    publish_wheel_rpm(back_right_speed, msg_in.speed, msg_in.timeout)
 
 def handle_configure(channel, method, header, body):
-	# Caleed when we receive message from configure queue
-	configuration.ParseFromString(body)
+    # Caleed when we receive message from configure queue
+    configuration = messages_pb2.LocomotionControlCommandConfigure()
+    configuration.ParseFromString(body)
 
-	if (configuration.target is 
-			message_pb2.LocomotionCommandConfigure.Configuration.STRAIGHT_CONFIG)
-		setup_config_timeout(front_left_pod, 0, msg_in.timeout)
-		setup_config_timeout(front_right_pod, 0, msg_in.timeout)
-		setup_config_timeout(back_left_pod, 0, msg_in.timeout)
-		setup_config_timeout(back_right_pod, 0, msg_in.timeout)
-	else if (configuration.target is
-			message_pb2.LocomotionCommandConfigure.Configuration.TURN_CONFIG)
-		setup_config_timeout(front_left_pod, 0.5, msg_in.timeout)
-		setup_config_timeout(front_right_pod, 0.5, msg_in.timeout)
-		setup_config_timeout(back_left_pod, 0.5, msg_in.timeout)
-		setup_config_timeout(back_right_pod, 0.5, msg_in.timeout)
-	else if (configuration.target is
-			message_pb2.LocomotionCommandConfigure.Configuration.STRAFE_CONFIG)
-		setup_config_timeout(front_left_pod, 0, msg_in.timeout)
-		setup_config_timeout(front_right_pod, 0, msg_in.timeout)
-		setup_config_timeout(back_left_pod, 0, msg_in.timeout)
-		setup_config_timeout(back_right_pod, 0, msg_in.timeout)
-	
-def setup_rpm_timeout(wheel, rpm, timeout, topic):
-	# Setup rpm and timeout for the wheel object
-	# while at the same time send the serialized message to 
-	# the emitting exchange
-	wheel.rpm = rpm
-	wheel.timeout = timeout
-	channel.basic_publish(exchange=exchange_emit,
-			routing_key=topic,
-			body=wheel.SerializeToString())
+    if (configuration.target is 
+            messages_pb2.LocomotionCommandConfigure.Configuration.STRAIGHT_CONFIG):
+        publish_wheel_pod_angle(Wheel.front_left, 0, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.front_right_pod, 0, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.back_left_pod, 0, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.back_right_pod, 0, msg_in.timeout)
+    elif (configuration.target is
+            messages_pb2.LocomotionCommandConfigure.Configuration.TURN_CONFIG):
+        publish_wheel_pod_angle(Wheel.front_left, 0.5, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.front_right, 0.5, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.back_left, 0.5, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.back_right, 0.5, msg_in.timeout)
+    elif (configuration.target is
+            messages_pb2.LocomotionCommandConfigure.Configuration.STRAFE_CONFIG):
+        publish_wheel_pod_angle(Wheel.front_left, 1, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.front_right, 1, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.back_left, 1, msg_in.timeout)
+        publish_wheel_pod_angle(Wheel.back_right, 1, msg_in.timeout)
+    
+def publish_wheel_rpm(wheel, rpm, timeout):
+    msg = messages_pb2.SpeedContolCommand()
+    msg.rpm = rpm
+    msg.timeout = timeout
+    topic = 'motorcontrol.locomotion.' + wheel.name + '.wheel_rpm'
+    channel.basic_publish(exchange=exchange_name,
+            routing_key=topic,
+            body=msg.SerializeToString())
+    print(topic)
+
+def publish_wheel_pod_angle(wheel, angle, timeout):
+    msg = messages_pb2.PositionContolCommand()
+    msg.position = angle
+    msg.timeout = timeout
+    topic = 'motorcontrol.locomotion.' + wheel.name + '.wheel_pod_pos'
+    channel.basic_publish(exchange=exchange_name,
+            routing_key=topic,
+            body=msg.SerializeToString())
 
 
-def setup_config_timeout(wheel, rpm, timeout, topic):
-	# Setup pod and timeout for the wheel object
-	# while at the same time serialized message to 
-	# the emitting exchange
-	wheel.pod = pod
-	wheel.timeout = timeout
-	channel.basic_publish(exchange=exchange_emit,
-			routing_key=topic,
-			body=wheel.SerializeToString())
-
+# Step #1: Connect to RabbitMQ using the default parameters
+parameters = pika.connection.URLParameters('amqp://guest:guest@localhost:5672/%2F')
+connection = pika.SelectConnection(parameters=parameters,
+                                   on_open_callback=on_connected)
+try:
+    # Loop so we can communicate with RabbitMQ
+    connection.ioloop.start()
+except KeyboardInterrupt:
+    # Gracefully close the connection
+    connection.close()
+    # Loop until we're fully closed, will stop on its own
+    connection.ioloop.start()
