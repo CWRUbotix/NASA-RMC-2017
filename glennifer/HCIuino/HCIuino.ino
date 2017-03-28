aa53f4f68cb995dd130c1d22757fe6fb3b524b4c
Michael Schaffer <mls278@case.edu>
2017-03-27 23:25:15 -0400

    Rewrite arduino message code using good C style

--- glennifer/HCIuino/HCIuino.ino
+++ glennifer/HCIuino/HCIuino.ino
@@ -1,97 +1,114 @@
+#include "RoboClaw.h"
+#include "math.h"
+
 #define ID_LBM  0
 #define ID_RBM  1
 #define ID_LFM  2
 #define ID_RFM  3
 
-#include "RoboClaw.h"
-#include "math.h"
+#define COMMAND_READ_SENSORS (0x01)
+#define COMMAND_SET_OUTPUTS (0x02)
+#define COMMAND_HCI_TEST    (0x5A)
 
-#define COMMAND_READ_SENSORS 0x01
-#define COMMAND_SET_OUTPUTS 0x02
-#define COMMAND_HCI_TEST    0x5A
+#define RESPONSE_HCI_TEST   (0xA5)
 
-#define RESPONSE_HCI_TEST   0xA5
+#define ADDRESS_RC_0 (0x80)
+#define ADDRESS_RC_1 (0x81)
+#define ADDRESS_RC_2 (0x82)
+#define ADDRESS_RC_3 (0x83)
 
-#define ADDRESS_RC_0 0x80
-#define ADDRESS_RC_1 0x81
-#define ADDRESS_RC_2 0x82
-#define ADDRESS_RC_3 0x83
+#define CMD_HEADER_SIZE (2)
+#define RPY_HEADER_SIZE (2)
 
-#define MAX_SENSORS 256
-
-struct message {
-  byte command;
-  byte len;
-  byte* data;
-};
-
-int state = 0;
-
-int num_sensors = 0;
-int sensor_ids[MAX_SENSORS];
-int sensor_data[MAX_SENSORS];
+#define FAULT_T uint16_t
+#define NO_FAULT (1)
+#define FAULT_FAILED_WRITE (2)
+#define FAULT_INCOMPLETE_HEADER (3)
+#define FAULT_CORRUPTED_HEADER (4)
+#define FAULT_INCOMPLETE_BODY (5)
+#define FAULT_CORRUPTED_BODY (6)
 
 RoboClaw roboclaw(&Serial1,10000);
 
 void setup() {
   roboclaw.begin(38400);
   SerialUSB.begin(9600);
+  roboclaw.SetConfig(ADDRESS_RC_0, 0x8063);
+  roboclaw.SetConfig(ADDRESS_RC_1, 0x8163);
+  roboclaw.SetConfig(ADDRESS_RC_2, 0x8263);
+  roboclaw.SetConfig(ADDRESS_RC_3, 0x8363);
+  roboclaw.SetM1EncoderMode(ADDRESS_RC_1, 0x81);
+  roboclaw.WriteNVM(ADDRESS_RC_0);
+  roboclaw.WriteNVM(ADDRESS_RC_1);
+  roboclaw.WriteNVM(ADDRESS_RC_2);
+  roboclaw.WriteNVM(ADDRESS_RC_3);
 }
 
 void loop() {
-  while(SerialUSB.available()) {
-    message ms = hciRead();
-    execute(ms);
+  while (true) {
+    FAULT_T retfault;
+    byte cmd[256];
+    hciWait();
+    retfault = hciRead(cmd);
+    if (retfault != NO_FAULT) {
+      // Enter sync mode
+      // When done:
+      continue;
+    }
+    // cmd is valid
+    execute(cmd);
   }
 }
 
-void execute(message m) {
-  byte response_data[MAX_SENSORS * 4]; // max-sized response buffer
-  switch(m.command) {
+void execute(byte cmd[]) {
+  byte rpy[256]; // max-sized response buffer
+  uint8_t type = cmd_type(cmd);
+  uint8_t num_sensors_requested;
+  uint8_t num_motors_requested;
+  FAULT_T retfault;
+  switch(type) {
     case COMMAND_HCI_TEST:
       SerialUSB.write(RESPONSE_HCI_TEST);
       break;
     case COMMAND_READ_SENSORS:
-      for(int i = 0; i < m.len/2; i++) {
-        short id =  (m.data[i*2+0] << 8) | m.data[i*2+1];
+      rpy_init(rpy, type);
+      num_sensors_requested = cmd_sense_num_sensors(cmd);
+      for(int i = 0; i < num_sensors_requested; i++) {
+        uint16_t id =  cmd_sense_sensor_id(cmd, i);
         
-        //uint8_t status;
-        //bool valid;
-        // short val = (short)roboclaw.ReadEncM1(id, &status, &valid); // This line causes everything to fail
-        //if (!valid){
-        //  val = 0; // TODO: handle this
-        //}
-        short val = 99;
-        
-        
-        response_data[i*4+0] = (byte)(id >> 8);
-        response_data[i*4+1] = (byte)id;
-        response_data[i*4+2] = (byte)(val >> 8);
-        response_data[i*4+3] = (byte)val;
-
-        message resp;
-        resp.command = COMMAND_READ_SENSORS;
-        resp.len = (m.len/2) * 4;
-        resp.data = response_data;
-        hciSend(resp);
+        uint8_t status;
+        bool valid = true;
+        int32_t val32 = roboclaw.ReadEncM1(ADDRESS_RC_1, &status, &valid);
+        int16_t val = (int16_t)val32;
+        if (!valid){
+          val = 0x8000; // Error code
+        }
+        bool overflow = rpy_sense_add_sensor(rpy, id, val);
+        if (overflow) {
+          // do nothing
+        }
+      }
+      rpy_finalize(rpy);
+      retfault = hciWrite(rpy);
+      if (retfault != NO_FAULT) {
+        // TODO: enter sync mode
       }
       break;
     case COMMAND_SET_OUTPUTS:
-      for(int i = 0; i < m.len/4; i++) {
-        short id = (m.data[i*4+0] << 8)|m.data[i*4+1];
-        short out = (m.data[i*4+2] << 8)|m.data[i*4+3];
-        setActuator(id, out);
+      rpy_init(rpy, type);
+      num_motors_requested = cmd_ctl_num_motors(cmd);
+      for(int i = 0; i < num_motors_requested; i++) {
+        uint16_t id = cmd_ctl_motor_id(cmd, i);
+        int16_t val = cmd_ctl_motor_val(cmd, i);
+        setActuator(id, val);
       }
-      message resp;
-      resp.command = COMMAND_SET_OUTPUTS;
-      resp.len = 1;
-      response_data[0] = 0;
-      resp.data = response_data;
-      hciSend(resp);
+      rpy_finalize(rpy);
+      retfault = hciWrite(rpy);
+      if (retfault != NO_FAULT) {
+        // TOCO: enter sync mode
+      }
+      break;
   }
-}
-void readSensors() {
-  
 }
 
 void setActuator(short ID, short val) {
@@ -153,28 +170,134 @@ void setActuator(short ID, short val) {
   }
 }
 
-bool hciSend(message m) {
-  if(SerialUSB.write(m.command) != 1) {
-    return false;
+void hciWait() {
+  while(!SerialUSB.available()) {}
+}
+
+FAULT_T hciWrite(byte rpy[]) {
+  uint8_t retval;
+  uint8_t len = rpy_len(rpy);
+  retval = SerialUSB.write(rpy,len+2);
+  if (retval != len+2) {
+    return FAULT_FAILED_WRITE;
   }
-  if(SerialUSB.write(m.len) != 1) {
-    return false;
+  return NO_FAULT;
+}
+
+FAULT_T hciRead(byte cmd[]) {
+  uint8_t retval;
+  retval = SerialUSB.readBytes(cmd, CMD_HEADER_SIZE);
+  if (retval != CMD_HEADER_SIZE) {
+    return FAULT_INCOMPLETE_HEADER;
   }
-  if(SerialUSB.write(m.data,m.len) == m.len) {
+  bool valid = cmd_check_head(cmd);
+  if (!valid) {
+    return FAULT_CORRUPTED_HEADER;
+  }
+  uint8_t len = cmd_len(cmd);
+  retval = SerialUSB.readBytes(cmd + CMD_HEADER_SIZE, len);
+  if (retval != len) {
+    return FAULT_INCOMPLETE_BODY;
+  }
+  valid = cmd_check_body(cmd);
+  if (!valid) {
+    return FAULT_CORRUPTED_BODY;
+  }
+  return NO_FAULT;
+}
+
+// Precondition: every message array has length at least 256
+
+bool cmd_check_head(byte cmd[]) {
+  return true;
+}
+
+uint8_t cmd_type(byte cmd[]) {
+  return cmd[0];
+}
+
+uint8_t cmd_len(byte cmd[]) {
+  return cmd[1];
+}
+
+bool cmd_check_body(byte cmd[]) {
+  return true;
+}
+
+uint8_t cmd_sense_num_sensors(byte cmd[]) {
+  return cmd_len(cmd)/2;
+}
+
+uint16_t cmd_sense_sensor_id(byte cmd[], uint8_t i) {
+  // i unchecked
+  return ((uint16_t)cmd[CMD_HEADER_SIZE + 2*i + 0] << 8) | cmd[CMD_HEADER_SIZE + 2*i + 1];
+}
+
+uint8_t cmd_ctl_num_motors(byte cmd[]) {
+  return cmd_len(cmd)/4;
+}
+
+uint16_t cmd_ctl_motor_id(byte cmd[], uint8_t i) {
+  // i unchecked
+  return ((uint16_t)cmd[CMD_HEADER_SIZE + 4*i + 0] << 8) | cmd[CMD_HEADER_SIZE + 4*i + 1];
+}
+
+int16_t cmd_ctl_motor_val(byte cmd[], uint8_t i) {
+  // i unchecked
+  return ((int16_t)cmd[CMD_HEADER_SIZE + 4*i + 2] << 8) | cmd[CMD_HEADER_SIZE + 4*i + 3];
+}
+
+void rpy_init(byte rpy[], uint8_t type) {
+  rpy[0] = type;
+  rpy[1] = 0; // len
+}
+
+uint8_t rpy_len(byte rpy[]) {
+  return rpy[1];
+}
+
+void rpy_set_len(byte rpy[], uint8_t new_len) {
+  rpy[1] = new_len;
+}
+
+bool rpy_sense_add_sensor(byte rpy[], uint16_t id, int16_t val) {
+  // size unchecked
+  uint8_t len = rpy_len(rpy);
+  if (len >= 252) {
     return true;
   }
+  byte *rpy_id_high_ptr = rpy + RPY_HEADER_SIZE + len + 0;
+  byte *rpy_id_low_ptr = rpy + RPY_HEADER_SIZE + len + 1;
+  byte *rpy_val_high_ptr = rpy + RPY_HEADER_SIZE + len + 2;
+  byte *rpy_val_low_ptr = rpy + RPY_HEADER_SIZE + len + 3;
+  *rpy_id_high_ptr = (byte)(id >> 8);
+  *rpy_id_low_ptr = (byte)id;
+  *rpy_val_high_ptr = (byte)((unsigned)val >> 8);
+  *rpy_val_low_ptr = (byte)val;
+  len += 4;
+  rpy_set_len(rpy, len);
   return false;
 }
 
-message hciRead() {
-  byte head[2];
-  SerialUSB.readBytes(head,2);
-  byte data[head[1]];
-  SerialUSB.readBytes(data,head[1]);
-  message out;
-  out.command = head[0];
-  out.len = head[1];
-  out.data = data;
-  return out;
+bool rpy_ctl_add_motor(byte rpy[], uint16_t id, int16_t val) {
+  // size unchecked
+  uint8_t len = rpy_len(rpy);
+  if (len >= 252) {
+    return true;
+  }
+  byte *rpy_id_high_ptr = rpy + RPY_HEADER_SIZE + len + 0;
+  byte *rpy_id_low_ptr = rpy + RPY_HEADER_SIZE + len + 1;
+  byte *rpy_val_high_ptr = rpy + RPY_HEADER_SIZE + len + 2;
+  byte *rpy_val_low_ptr = rpy + RPY_HEADER_SIZE + len + 3;
+  *rpy_id_high_ptr = (byte)(id >> 8);
+  *rpy_id_low_ptr = (byte)id;
+  *rpy_val_high_ptr = (byte)((unsigned)val >> 8);
+  *rpy_val_low_ptr = (byte)val;
+  len += 4;
+  rpy_set_len(rpy, len);
+  return false;
 }
+
+void rpy_finalize(byte rpy[]) {}
+
 
