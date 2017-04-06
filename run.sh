d6e5956fe5651aac87714aa51d128c9605fa8efa
Steven Leonis <Spwizzard2@gmail.com>
2017-04-06 14:44:53 -0400

    Fixed line in run.sh Please enter the commit message for your changes. Lines starting

--- run.sh
+++ run.sh
@@ -2,17 +2,13 @@
 #build protobuf
 ./build-pb.sh
 
-#make sure stuff is compiled
-mvn compile
-
 #now actually start the things
 cd glennifer/HCI/src/main/java/com/cwrubotix/glennifer/hci
 mvn compile
-
 mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.hci.ModuleMain" &
 cd ../../../../../../../..
-cd robot_state/src/main/java/com/gwrubotix/glennifer/robot_state
 
+cd robot_state/src/main/java/com/gwrubotix/glennifer/robot_state
 mvn compile
 mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.robot_state.StateModule" &
 cd ../../../../../../../..
