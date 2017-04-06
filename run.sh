eb70a32cf4d7e51a4977433588ec53b36baef102
Steven Leonis <Spwizzard2@gmail.com>
2017-04-06 14:24:06 -0400

    run.sh actually added to the commit...

---
+++ run.sh
@@ -0,0 +1,19 @@
+
+#build protobuf
+./build-pb.sh
+
+#make sure stuff is compiled
+mvn compile
+
+#now actually start the things
+cd glennifer/HCI/src/main/java/com/cwrubotix/glennifer/hci
+mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.hci.ModuleMain" &
+cd ../../../../../../../..
+
+cd robot_state/src/main/java/com/gwrubotix/glennifer/robot_state
+mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.robot_state.StateModule" &
+cd ../../../../../../../..
+
+cd motor_dispatch/src/python
+python3 locomotion.py &
+
