caa0aab46eea70db66adbd75a0bc3fc2335f1ef0
Michael Schaffer <mls278@case.edu>
2017-04-06 15:08:00 -0400

    Make run work

--- run.sh
+++ run.sh
@@ -2,16 +2,21 @@
 #build protobuf
 ./build-pb.sh
 
+# copy config
+cp -r config glennifer/HCI/
+cp -r config glennifer/robot_state
+cp -r config glennifer/motor_dispatch/src/python
+
 #now actually start the things
-cd glennifer/HCI/src/main/java/com/cwrubotix/glennifer/hci
+cd glennifer/HCI
 mvn compile
 mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.hci.ModuleMain" &
-cd ../../../../../../../..
+cd ..
 
-cd robot_state/src/main/java/com/gwrubotix/glennifer/robot_state
+cd robot_state
 mvn compile
 mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.robot_state.StateModule" &
-cd ../../../../../../../..
+cd ..
 
 cd motor_dispatch/src/python
 python3 locomotion.py &
