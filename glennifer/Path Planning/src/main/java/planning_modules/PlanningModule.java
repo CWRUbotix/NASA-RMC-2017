de6cce41ab9339b214f47b0e2c6f95366f5c24bd
Shota <srn24@case.edu>
2017-04-10 16:02:23 -0400

    Some messaging stuff for path planning

--- glennifer/Path Planning/src/main/java/planning_modules/PlanningModule.java
+++ glennifer/Path Planning/src/main/java/planning_modules/PlanningModule.java
@@ -3,6 +3,7 @@ package planning_modules;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 import com.rabbitmq.client.AMQP;
@@ -31,6 +32,18 @@ public class PlanningModule {
 
 		subscribe(connection);
 		publish(connection, "Hello");
+
+		boolean run = true;
+		Scanner scan = new Scanner(System.in);
+		String input;
+		while (run) {
+			input = scan.nextLine();
+			if (input.equals("quit"))
+				run = false;
+			else
+				publish(connection, input);
+		}
+		scan.close();
 		Thread.sleep(10000000);
 	}
 
@@ -64,17 +77,16 @@ public class PlanningModule {
 	 * @throws java.io.IOException
 	 * @throws TimeoutException
 	 */
-	private static void publish(Connection connection, String message)
-			throws java.io.IOException, TimeoutException {
+	private static void publish(Connection connection, String message) throws java.io.IOException, TimeoutException {
 		Channel channel = connection.createChannel();
 
 		channel.exchangeDeclare(COMMAND_EXCHANGE, "topic", true);
 
-		channel.basicPublish(COMMAND_EXCHANGE, "", null, message.getBytes());
+		channel.basicPublish(COMMAND_EXCHANGE, "HighCommand", null, message.getBytes());
 		System.out.println(" [x] Sent '" + message + "'");
 
 		channel.close();
-		connection.close();
+		// connection.close();
 	}
 
 	/**
@@ -90,7 +102,8 @@ public class PlanningModule {
 		String queueName = channel.queueDeclare().getQueue();
 		channel.queueBind(queueName, COMMAND_EXCHANGE, "HighCommand");
 
-		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+		// System.out.println(" [*] Waiting for messages. To exit press
+		// CTRL+C");
 
 		Consumer consumer = new DefaultConsumer(channel) {
 			@Override
@@ -98,7 +111,7 @@ public class PlanningModule {
 					byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
 				System.out.println(" [x] Received '" + message + "'");
-				
+
 				updateState(message);
 			}
 		};
@@ -107,39 +120,46 @@ public class PlanningModule {
 
 	/**
 	 * Interprets the information from the state modules
+	 * 
 	 * @param info
 	 */
 	private static void updateState(String info) {
-		//TODO put stuff about how to update the state info here
+		// TODO put stuff about how to update the state info here
 	}
-	
+
 	/**
 	 * Parses the information from received HighLevelCommands
+	 * 
 	 * @param command
 	 */
-	private static void receiveCommand(String command){
+	private static void receiveCommand(String command) {
 		Queue<MidLevelCommand> command_queue = new LinkedList<MidLevelCommand>();
-		
+
 		String[] contents = command.split("\\.");
 		HighLevelCommand high_command = translate(command);
 		String type = contents[0];
-		
+
 		if (type.equals("Deposition")) {
 			// TODO put stuff here for deposition commands
-//			command_queue = Deposition.receiveCommand(stuff);
+			// command_queue = Deposition.receiveCommand(stuff);
 		} else if (type.equals("Excavation")) {
 			// TODO put stuff here excavation commands
-//			command_queue = Excavation.receiveCommand(stuff);
+			// command_queue = Excavation.receiveCommand(stuff);
 		} else if (type.equals("Locomotion")) {
 			// TODO put stuff here locomotion commands
-//			command_queue = Locomotion.receiveCommand(stuff);
+			// command_queue = Locomotion.receiveCommand(stuff);
 		}
 	}
-	
-	private static HighLevelCommand translate(String command){
-		//This is just a placeholder until I know how the information will be represented
+
+	private static HighLevelCommand translate(String command) {
+		// This is just a placeholder until I know how the information will be
+		// represented
 		HighLevelCommand temp = new HighLevelCommand(1, 5f);
 		return temp;
+	}
+	
+	message killMe{
+		
 	}
 
 }
