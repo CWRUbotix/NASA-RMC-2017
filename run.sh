54c7ba22f033be7c9a37e62682a3e92f46787484
Steven Leonis <Spwizzard2@gmail.com>
2017-04-13 14:53:14 -0400

    Run.sh should print output to log files now

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+
+mkdir -p glennifer_logs/$(date +%Y_%m_%d_%H_%M_%S)
+
 cd /home/cwrubotix/workspace/NASA-RMC-2017/
 
 #build protobuf
@@ -11,16 +14,16 @@ cp -r config glennifer/motor_dispatch/src/python
 #now actually start the things
 cd glennifer/HCI
 mvn compile
-mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.hci.ModuleMain" &
+mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.hci.ModuleMain" &> /home/glennifer_logs/ModuleMainOutput.log &
 cd ..
 
 cd robot_state
 mvn compile
-mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.robot_state.StateModule" &
+mvn exec:java -Dexec.mainClass="com.cwrubotix.glennifer.robot_state.StateModule" &> /home/glennifer_logs/StateModuleOutput.log &
 cd ..
 
 cd motor_dispatch/src/python
-python3 locomotion.py &
+python3 locomotion.py &> /home/glennifer_logs/LocomotionPyOutput.log &
 cd ../../..
 
 cd client-cameras
