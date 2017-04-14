8df44340e2f6e86b62945fa97be42eca0f96c341
Seohyun Jung <jsh318900@gmail.com>
2017-04-13 20:10:42 -0400

    Finally fixed reverse funcion in ModuleMain

--- glennifer/HCI/src/main/java/com/cwrubotix/glennifer/hci/ModuleMain.java
+++ glennifer/HCI/src/main/java/com/cwrubotix/glennifer/hci/ModuleMain.java
@@ -466,9 +466,9 @@ public class ModuleMain {
 					a.override = true;
 					a.hold = true;
 					if (id % 2 == 0) {
-                        a.targetValue = -(scc.getRpm() / 60.0F) * 270 * 4096 / 100.0F; // Here 0 thru 3
+                        a.targetValue = -(scc.getRpm() / 60.0F) * 270 * 4096 / 100.0F;
                     } else {
-                        a.targetValue = (scc.getRpm() / 60.0F) * 270 * 4096 / 100.0F; // Here 0 thru 3
+                        a.targetValue = (scc.getRpm() / 60.0F) * 270 * 4096 / 100.0F;
                     }
                     System.out.println("target value = " + a.targetValue);
 					a.type = HardwareControlInterface.ActuationType.AngVel;
@@ -547,28 +547,28 @@ public class ModuleMain {
 							.build();
 					channel.basicPublish("amq.topic", "sensor.locomotion.back_right.wheel_rpm", null, msg.toByteArray());
 				} else if(sensorDataID == 4){
-					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691;
+					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691 +360.38;
 					Messages.PositionUpdate msg = Messages.PositionUpdate.newBuilder()
 							.setPosition((float)value)
 							.setTimestamp(unixTime)
 							.build();
 					channel.basicPublish("amq.topic", "sensor.locomotion.front_left.wheel_pod_pos", null, msg.toByteArray());
 				} else if(sensorDataID == 5){
-					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691;
+					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691 + 360.38;
 					Messages.PositionUpdate msg = Messages.PositionUpdate.newBuilder()
 							.setPosition((float)value)
 							.setTimestamp(unixTime)
 							.build();
 					channel.basicPublish("amq.topic", "sensor.locomotion.front_right.wheel_pod_pos", null, msg.toByteArray());
 				} else if(sensorDataID == 6){
-					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691;
+					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691 + 360.38;
 					Messages.PositionUpdate msg = Messages.PositionUpdate.newBuilder()
 							.setPosition((float)value)
 							.setTimestamp(unixTime)
 							.build();
 					channel.basicPublish("amq.topic", "sensor.locomotion.back_left.wheel_pod_pos", null, msg.toByteArray());
 				} else if(sensorDataID == 7){
-					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691;
+					value = 180.2324 / Math.PI * Math.asin((((3.3 / 1024 * (1023 - value) - 0.04624) / 0.79547 - 1.03586)) / 1.50175) - 316.63691 + 360.38;
 					Messages.PositionUpdate msg = Messages.PositionUpdate.newBuilder()
 							.setPosition((float)value)
 							.setTimestamp(unixTime)
