36ca3dd6daa61466d92dfe8d91992f58eee7ad2a
Michael Schaffer <mls278@case.edu>
2017-04-16 12:45:04 -0400

    Updates to cam sending code

--- client-cameras/client-cam-send.py
+++ client-cameras/client-cam-send.py
@@ -3,48 +3,31 @@ import numpy as np
 import pika
 import sys
 import time
-import yaml
 
-with open('config/connection.yml') as connection_config_file:
-    connection_config = yaml.safe_load(connection_config_file)
-    amqp_server_addr = connection_config['server-addr']
-    amqp_server_user = connection_config['server-user']
-    amqp_server_pass = connection_config['server-pass']
-    amqp_exchange_name = connection_config['exchange-name']
-
-credentials = pika.PlainCredentials(amqp_server_user, amqp_server_pass)
-parameters = pika.ConnectionParameters(amqp_server_addr,  5672, '/', credentials=credentials)
+#Change demo to guest or local host, or another user, password tuple
+#Change ip address to the one for the server running the receive code
+#172.20.36.235
+#credentials = pika.PlainCredentials('guest', 'guest')
+#parameters = pika.ConnectionParameters('localhost',  5672, '/', credentials=credentials)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='amq.topic',  type = 'topic', durable = True)
 
 #Sends a message
 def send1(msg):
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue = 'cam1')
-    channel.basic_publish(exchange = '',  routing_key = 'cam1',  body = msg)
+    channel.basic_publish(exchange = 'amq.topic',  routing_key = 'camera.one',  body = msg)
     
 def send2(msg):
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue = 'cam2')
-    channel.basic_publish(exchange = '',  routing_key = 'cam2',  body = msg)
+    channel.basic_publish(exchange = 'amq.topic',  routing_key = 'camera.two',  body = msg)
     
 def send3(msg):
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue = 'cam3')
-    channel.basic_publish(exchange = '',  routing_key = 'cam3',  body = msg)
+    channel.basic_publish(exchange = 'amq.topic',  routing_key = 'camera.three',  body = msg)
     
 def send4(msg):
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue = 'cam4')
-    channel.basic_publish(exchange = '',  routing_key = 'cam4',  body = msg)
+    channel.basic_publish(exchange = 'amq.topic',  routing_key = 'camera.four',  body = msg)
     
 def send5(msg):
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue = 'cam5')
-    channel.basic_publish(exchange = '',  routing_key = 'cam5',  body = msg)
+    channel.basic_publish(exchange = 'amq.topic',  routing_key = 'camera.five',  body = msg)
     
 #Camera Capture
 #Change Capture Device Index to correspinding index
@@ -52,25 +35,25 @@ def send5(msg):
 # cam1.set(3,  480)
 #cam1.set(4, 320)
 cam1 = cv2.VideoCapture(0)
-cam1.set(3, 240)
-cam1.set(4, 160)
+cam1.set(3, 480)
+cam1.set(4, 420)
 cam2 = cv2.VideoCapture(1)
-cam2.set(3, 240)
-cam2.set(4, 160)
+cam2.set(3, 480)
+cam2.set(4, 420)
 cam3 = cv2.VideoCapture(2)
-cam3.set(3, 240)
-cam3.set(4, 160)
+cam3.set(3, 480)
+cam3.set(4, 420)
 cam4 = cv2.VideoCapture(3)
-cam4.set(3, 240)
-cam4.set(4, 160)
+cam4.set(3, 480)
+cam4.set(4, 420)
 cam5 = cv2.VideoCapture(4)
-cam5.set(3, 240)
-cam5.set(4, 160)
+cam5.set(3, 480)
+cam5.set(4, 420)
 def getCam1():
     if (cam1.isOpened() is True):
         ret,  frame1 = cam1.read()
-        imr1 = cv2.cvtColor(frame1,  cv2.COLOR_BGR2RGB)
-        img1 = cv2.imencode('.ppm',  imr1)[1].tostring()
+        #imr1 = cv2.cvtColor(frame1,  cv2.COLOR_BGR2RGB)
+        img1 = cv2.imencode('.ppm',  frame1)[1].tostring()
         send1(img1)
     else:
          c= 1
@@ -78,8 +61,8 @@ def getCam1():
 def getCam2():
     if (cam2.isOpened() is True):
         ret,  frame2 = cam2.read()
-        imr2 = cv2.cvtColor(frame2,  cv2.COLOR_BGR2RGB)
-        img2 = cv2.imencode('.ppm',  imr2)[1].tostring()
+        #imr2 = cv2.cvtColor(frame2,  cv2.COLOR_BGR2RGB)
+        img2 = cv2.imencode('.ppm',  frame2)[1].tostring()
         send2(img2)
     else:
         c= 1
@@ -87,8 +70,8 @@ def getCam2():
 def getCam3():
     if (cam3.isOpened() is True):
         ret,  frame3 = cam3.read()
-        imr3 = cv2.cvtColor(frame3,  cv2.COLOR_BGR2RGB)
-        img3 = cv2.imencode('.ppm',  imr3)[1].tostring()
+        #imr3 = cv2.cvtColor(frame3,  cv2.COLOR_BGR2RGB)
+        img3 = cv2.imencode('.ppm',  frame3)[1].tostring()
         send3(img3)
     else:
          c= 1
@@ -96,8 +79,8 @@ def getCam3():
 def getCam4():
     if (cam4.isOpened() is True):
         ret,  frame4 = cam4.read()
-        imr4 = cv2.cvtColor(frame4,  cv2.COLOR_BGR2RGB)
-        img4 = cv2.imencode('.ppm',  imr4)[1].tostring()
+        #imr4 = cv2.cvtColor(frame4,  cv2.COLOR_BGR2RGB)
+        img4 = cv2.imencode('.ppm',  frame4)[1].tostring()
         send4(img4)
     else:
          c= 1
@@ -105,8 +88,8 @@ def getCam4():
 def getCam5():
     if (cam5.isOpened() is True):
         ret,  frame5 = cam5.read()
-        imr5 = cv2.cvtColor(frame5,  cv2.COLOR_BGR2RGB)
-        img5 = cv2.imencode('.ppm',  imr5)[1].tostring()
+        #imr5 = cv2.cvtColor(frame5,  cv2.COLOR_BGR2RGB)
+        img5 = cv2.imencode('.ppm',  frame5)[1].tostring()
         send5(img5)
     else:
          c= 1
@@ -123,4 +106,4 @@ while(True):
     getCam4()
     #Camera Five Update
     getCam5()
-    time.sleep(1)
+    time.sleep(0.1)
