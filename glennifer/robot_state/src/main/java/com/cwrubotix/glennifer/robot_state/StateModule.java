9be079a0e84e367c13d62579693d0929821562e0
Michael Schaffer <mls278@case.edu>
2017-05-07 13:07:19 -0400

    whoops

--- glennifer/robot_state/src/main/java/com/cwrubotix/glennifer/robot_state/StateModule.java
+++ glennifer/robot_state/src/main/java/com/cwrubotix/glennifer/robot_state/StateModule.java
@@ -212,8 +212,10 @@ public class StateModule {
                     handleConveyorRpmUpdate(body);
                 } else if (sensorString.equals("translation_pos")) {
                     handleConveyorTranslationPosUpdate(body);
-                }  else if (sensorString.equals("arm_pos")) {
+                }  else if (sensorString.equals("arm_pos_a")) {
                     handleArmPosUpdate(body);
+                }  else if (sensorString.equals("arm_pos_b")) {
+                    // do nothing for now
                 } else if (sensorString.equals("arm_limit_extended")) {
                     String sideString = keys[3];
                     ExcavationState.Side side;
@@ -287,10 +289,8 @@ public class StateModule {
                         return;
                     }
                     handleDumpLoadUpdate(loadcell, body);
-                } else if (sensorString.equals("arm_pos_a")) {
+                } else if (sensorString.equals("arm_pos")) {
                     handleDumpPosUpdate(body);
-                } else if (sensorString.equals("arm_pos_b")) {
-                    // do nothing for now
                 } else if (sensorString.equals("dump_limit_extended")) {
                     String sideString = keys[3];
                     DepositionState.Side side;
