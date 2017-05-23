486f4d69ac39365cdde98ec4b7d11b9d64e88eed
Michael Schaffer <mls278@case.edu>
2017-05-23 14:57:27 -0400

    Correct java path

--- runglenn.sh
+++ runglenn.sh
@@ -7,18 +7,18 @@ cd /home/ubuntu/workspace/NASA-RMC-2017/
 
 # start the things
 #cd glennifer/HCI
-#nohup java -cp target/hci-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.hci.ModuleMain &> "/home/ubuntu/logs/$datestring/ModuleMainOutput.log" &
+#nohup /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/java -cp target/hci-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.hci.ModuleMain &> "/home/ubuntu/logs/$datestring/ModuleMainOutput.log" &
 #cd ..
 
 cd glennifer
 cd robot_state
-nohup java -cp target/robot_state-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.robot_state.StateModule &> "/home/ubuntu/logs/$datestring/StateModuleOutput.log" &
+nohup /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/java -cp target/robot_state-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.robot_state.StateModule &> "/home/ubuntu/logs/$datestring/StateModuleOutput.log" &
 cd ..
 
 sleep 2
 
 cd autodrill
-nohup java -cp target/autodrill-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.autodrill.AutoDrillModule &> "/home/ubuntu/logs/$datestring/AutoDrillModuleOutput.log" &
+nohup /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/java -cp target/autodrill-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.autodrill.AutoDrillModule &> "/home/ubuntu/logs/$datestring/AutoDrillModuleOutput.log" &
 cd ..
 
 cd motor_dispatch/src/python
